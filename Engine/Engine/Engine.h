86b9cbe48e5b58bcffe6d77378c6aa0fc29d3b60
FelForest <fel.forest3910@gmail.com>
2025-01-17 11:17:38 +0900

    [Add] Vector2
    operator Overeloading

---
+++ Engine/Engine/Engine.h
@@ -0,0 +1,61 @@
+﻿#pragma once
+
+#include "Core.h"
+
+struct KeyState
+{
+	
+	bool isKeyDown = false;
+
+	bool wasKeyDown = false;
+
+};
+
+enum class CursorType
+{
+	Nocursor,
+	SolidCursor,
+	NormalCursor
+};
+
+class Engine
+
+{
+public :
+	Engine();
+	
+	virtual ~Engine();
+
+	void Run();
+
+	bool GetKey(int key);
+	bool GetKeyDown(int key);
+	bool GetKeyUp(int key);
+
+	void QuitGame();
+
+protected:
+
+	void ProcessInput();
+
+	void Update();
+
+	void Draw();
+
+	void SavePreviouseKeyStates();
+
+protected:
+
+	float targetFrameRate = 60.0f;
+
+	float targetOneFrameTime = 0.0f;
+
+	bool quit = false;
+
+	KeyState keyState[256];
+
+	bool shouldUpdate = true;
+
+
+private:
+};
---
+++ Engine/Math/Vector2.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include <PreCompiledHeader.h>
+
+class Vector2
+{
+public:
+	
+	Vector2(int x = 0, int y = 0);
+	~Vector2() = default;
+
+	// 연산자 오버로딩 
+	Vector2 operator+(const Vector2& other);
+	Vector2 operator-(const Vector2& other);
+	bool operator==(const Vector2& other);
+	bool operator!=(const Vector2& other);
+
+	// 내적
+	int dot(const Vector2& other) const;
+public:
+	int x = 0;
+	int y = 0;
+};
